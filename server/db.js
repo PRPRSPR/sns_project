1578706ae36488b3145fe2f340b410c5e162063b
PRPRSPR <s.subin.0208@gmail.com>
2025-05-19 17:26:17 +0900

    250519 - db정보 수정

--- server/db.js
+++ server/db.js
@@ -1,12 +1,5 @@
 const mysql = require('mysql2');
 
-// const pool = mysql.createPool({
-//     host: 'localhost',
-//     user: 'root',
-//     password: 'test1234',
-//     database: 'sns_project_db'
-// });
-
 const pool = mysql.createPool({
     host: process.env.host,
     user: process.env.user,
